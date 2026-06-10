32523138b1a8cf79739b6613c51e73e6d330bb0f
Felipe Gomes <felipegomes8819@gmail.com>
2026-06-10 16:48:23 -0300

    10/06

--- ReactJS/filmes/src/pages/login/Login.jsx
+++ ReactJS/filmes/src/pages/login/Login.jsx
@@ -1,37 +1,82 @@
 import "./login.css";
 import Botao from "../../components/botao/Botao";
 import Logo from "../../assets/img/logo.svg";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UsuarioContext } from "../../context/UsuarioContext";
 import { useNavigate } from "react-router-dom";
 import { Alerta } from "../../components/alerta/Alerta";
+import api from "../../services/Services";
+import { jwtDecode } from "jwt-decode";
 
 const Login = () => {
-  const { setUsuario } = useContext(UsuarioContext);
-  const [email, setEmail] = useState("");
   const navigate = useNavigate();
+  const { usuario, setUsuario } = useContext(UsuarioContext);
+  const [novoUsuario, setNovoUsuario] = useState("");
+  const [senha, setSenha] = useState("");
 
-  const logar = (e) => {
+  useEffect(() => {
+    const logado = JSON.parse(localStorage.getItem("usuario"));
+    if (logado) {
+      setUsuario(logado);
+      navigate("/generos");
+    }
+  }, []);
+
+
+
+  const loginUser = async (e) => {
     e.preventDefault();
 
-    if (!email.trim()) {
+    const email = novoUsuario.trim();
+    const senhaDigitada = senha.trim();
+
+    if (senhaDigitada.length === 0 || email.length === 0) {
       Alerta({
-        title: 'Login',
-        text: 'Digite um e-mail válido',
-        icon: 'warning',
-        confirmButtonText: 'OK'
+        title: "Campo obrigatório",
+        text: "Por favor, insira seu e-mail e senha antes de continuar.",
+        icon: "warning",
+        confirmButtonColor: "#CC3F55",
+        confirmButtonText: "Ok",
       });
       return;
     }
+    const dadosLogin = {
+      Email: email,
+      Senha: senhaDigitada,
+    };
 
-    setUsuario(email);
-    localStorage.setItem("usuario", JSON.stringify(email));
-    setEmail("");
+    try {
+      const retornoAPI = await api.post("/Login", dadosLogin);
+
+      const Token = retornoAPI.data.token;
+      const usuarioDecoder = jwtDecode(Token);
 
 
-    navigate("/filmes");
+      setUsuario(usuarioDecoder)
+      localStorage.setItem("usuario", JSON.stringify(usuarioDecoder));
+      setNovoUsuario("");
+      setSenha("");
+      Alerta({
+        title: "Login bem-sucedido",
+        text: "Bem-vindo de volta!",
+        icon: "success",
+        confirmButtonColor: "#CC3F55",
+      });
+      navigate("/generos");
+    }
+    catch (err) {
+      console.error("Erro detalhado retornado da API:", err.response?.data);
 
+      Alerta({
+        title: "Erro no login",
+        text: err.response?.data || "E-mail ou senha incorretos.",
+        icon: "error",
+        confirmButtonColor: "#CC3F55",
+      });
+    }
   };
+
+
 
   return (
     <>
@@ -40,22 +85,22 @@ const Login = () => {
         <section className="section_login">
           <img src={Logo} alt="Logo do Filmoteca" />
 
-          <form onSubmit={logar} className="form_login">
+          <form onSubmit={loginUser} className="form_login">
             <h1>Login</h1>
             <div className="campos_login">
               <div className="campo_input">
                 <label htmlFor="email">Email:</label>
                 <input
                   type="email"
-                  value={email}
+                  value={novoUsuario}
                   name="email"
                   placeholder="Digite seu e-mail"
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => setNovoUsuario(e.target.value)}
                 />
               </div>
               <div className="campo_input">
                 <label htmlFor="senha">Senha:</label>
-                <input type="password" name="senha" placeholder="Digite sua senha" />
+                <input type="password" name="senha" value={senha} onChange={(e) => setSenha(e.target.value)} placeholder="Digite sua senha" />
               </div>
             </div>
 
